c1986f2a901b9e8935f0b9c2b828eb4634b11cd3
aaronjangel <thatoneguy@aaronjangel.us>
2018-03-01 00:09:17 -0500

    simplified installation

--- bashrc/install.sh
+++ bashrc/install.sh
@@ -1,35 +1,12 @@
 #!/bin/sh
 
-if [ -f ~/.bashrc.d/.enabled ]; then
-  echo It appears that processing of \~/.bashrc.d/ has already been enabled. Do
-  echo you want to reinstall it?
-  echo
-  echo -n Reinstall? [y/N]: 
-  read reinstall
-
-  echo
-
-  case $reinstall in
-    y|Y)
-      echo Continuing with installation.
-      echo
-      ;;
-    *)
-      echo Installation cancelled at user request.
-      exit 1
-      ;;
-  esac
+if [ $(grep -c bashrc.d ~/.bashrc) != 0 ]; then
+  echo It appears that processing of \~/.bashrc.d/ has already been enabled.
+  exit 1
 fi
 
-if [ "$(pwd)" != "$HOME" ]; then
-  exec sh -c "cd $HOME; exec $(pwd)/${0#./}"
-fi
-
-echo Creating ~/.bashrc.d directory...
-mkdir -p .bashrc.d
-
-echo Patching ~/.bashrc...
-cat <<EOF > .bashrc
+mkdir -p ~/.bashrc.d
+cat <<EOF > ~/.bashrc
 
 # More bashrc files
 # You may want to put additional bashrc functionality in separate
@@ -43,9 +20,4 @@ for file in ~/.bashrc.d/*; do
 done
 EOF
 
-echo <<EOF
-Done.
-
-Your .bashrc file has been modified to source files marked as executable in
-the \~/.bashrc.d/ directory. Please open a new shell to make this effective.
-EOF
+echo Done.
